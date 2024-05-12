7cee39c140610115d2b66d60b6b24efe9f70bc1a
dchernyshev <dchernyshev@griddynamics.com>
2024-05-12 15:40:12 +0200

    Add Item class and relevant migrations

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
     def test_homepage_returns_correct_html(self):
@@ -11,3 +12,21 @@ class HomePageTest(TestCase):
         todo_text = "A new list item"
         response = self.client.post("/", data={"item_text": todo_text})
         self.assertContains(response, todo_text)
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(second_saved_item.text, "Item the second")
