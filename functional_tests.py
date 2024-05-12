20f3592a6e0a238b8bc127f788bf9d715a697bf0
dchernyshev <dchernyshev@griddynamics.com>
2024-05-12 14:53:16 +0200

    FT now checks for a few additions. Still erronously

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    # Helper methods
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_todo_list(self):
         # User goes to check out the home page
         self.browser.get("http://localhost:8000")
@@ -31,17 +37,18 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertTrue(
-            any(row.text == "1. Buy peacock feathers" for row in rows),
-            "New to-do item did not appear in the table"
-            )
+        self.check_for_row_in_list_table("1. Buy peacock feathers")
 
         # There is still a text box inviting her too add another item.
         # She enters "Use peacock feathers to make a fly"
-
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
         # The page updates again, and now shows both items on her lists
+        self.check_for_row_in_list_table("1. Buy peacock feathers")
+        self.check_for_row_in_list_table("2. Use peacock feathers to make a fly")
 
         # Satisfied, she goes back to sleep
 
--- lists/tests.py
+++ lists/tests.py
@@ -6,3 +6,8 @@ class HomePageTest(TestCase):
     def test_homepage_returns_correct_html(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
+    
+    def test_can_save_a_POST_request(self):
+        todo_text = "A new list item"
+        response = self.client.post("/", data={"item_text": todo_text})
+        self.assertContains(response, todo_text)
